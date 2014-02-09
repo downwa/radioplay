c3a35621f87e48daf897472dc482625a48e5c623
root <root@warrenlap.(none)>
2014-02-08 22:05:38 -0900

    Routing to pi

---
+++ forward_pi.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo echo 1 > /proc/sys/net/ipv4/ip_forward
+sudo iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
+sudo iptables -A FORWARD -i eth0 -o wlan0 -j ACCEPT
+sudo iptables -A FORWARD -i wlan0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
