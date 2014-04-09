021473aa531494e9915281e42914404b3e6cc2ff
Warren Downs <wdowns@choggiung.com>
2014-04-08 22:20:57 -0800

    Improved infinitepipe

---
+++ infinitepipe.cc
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+int main(int argc, char **argv) {
+	char infile[1024];
+	FILE *fpout=stdout;
+	if(argc < 2) {
+		fprintf(stderr,"Purpose: infinitepipe continuously opens and reads from the source pipe,\n");
+		fprintf(stderr,"         closing and reopening it whenever EOF or error occurs.  As it\n");
+		fprintf(stderr,"         reads, it copies the data to stdout, NOT closing stdout when\n");
+		fprintf(stderr,"         it closes the source pipe.  Optionally, a destination pipe or\n");
+		fprintf(stderr,"         file may be specified as the second argument.\n");
+		fprintf(stderr,"Usage: %s [source fifo] {[target fifo/file]}\n",argv[0]);
+		exit(-1);
+	}
+	strncpy(infile,argv[1],sizeof(infile)-1);
+	if(argc > 2) {
+		fpout=fopen(argv[2],"wb");
+		if(!fpout) { fprintf(stderr,"Error opening output '%s': %s\n",argv[2],strerror(errno)); exit(1); }
+	}
+	while(true) {
+		FILE *fpin=fopen(infile,"rb");
+		size_t olen;
+		while(!feof(fpin) && !ferror(fpin)) {
+			unsigned char buf[4000];
+			size_t ilen=fread(buf, sizeof(buf), 1, fpin);
+			if(ilen<=0) { break; }
+			size_t rem=ilen,ofs=0;
+			do {
+				olen=fwrite(&buf[ofs], rem, 1, fpout); fflush(fpout);
+				if(olen<rem || ferror(fpout)) {
+					fprintf(stderr,"Short write: %d < %d\n",olen,rem);
+					clearerr(fpout);
+				}
+				rem-=olen; ofs+=olen;
+			} while(rem>0);
+		}
+		fclose(fpin);
+	}	
+}
